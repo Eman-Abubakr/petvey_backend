223aa469514402b9133b38ee605e3f5376d3a9ed
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2021-12-02 19:09:35 -0500

    cors

--- config/app.js
+++ config/app.js
@@ -37,6 +37,14 @@ var app = express();
 // Enables cors.
 app.use(cors());
 app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // initialize passport
 app.use(passport.initialize());
