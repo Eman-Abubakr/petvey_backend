54a7f0512711e37ec63034d4769395a87d6b6f3f
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2021-12-04 10:11:38 -0500

    cors options

--- config/app.js
+++ config/app.js
@@ -41,7 +41,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 var corsOptions = {
-  origin: "*",
+  origin: "http://localhost:4200",
+  methods: "GET,PUT,POST,DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
