c2790dc4d9430c22c2e41e4715b3862d6275ce9c
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2021-12-04 09:16:29 -0500

    cors

--- config/app.js
+++ config/app.js
@@ -40,7 +40,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, Authorization, Content-Type, Accept"
+    "Origin, Authorization, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
   next();
 });
