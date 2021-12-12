cb6c1cb1ab58b2744abaeced75fa43d6b6344ea2
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2021-12-12 16:00:13 -0500

    cors

--- config/app.js
+++ config/app.js
@@ -45,7 +45,6 @@ var corsOptions = {
   methods: "GET,PUT,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
-  preflightContinue: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
