7d8bad92d1559d9bb56d8a21459d34368aaa871a
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2021-12-12 09:27:11 -0500

    cors

--- config/app.js
+++ config/app.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 var corsOptions = {
-  origin: "*",
+  origin: true,
   methods: "GET,PUT,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
