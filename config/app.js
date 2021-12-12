b363e954029543ab1bc5595016436b552cd9ae06
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2021-12-12 16:16:42 -0500

    c

--- config/app.js
+++ config/app.js
@@ -45,7 +45,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, authorization"
+    "Origin, X-Requested-With, Content-Type, authorization"
   );
   next();
 });
